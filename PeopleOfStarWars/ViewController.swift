1885f8e12a5a5fedad7389b1754f95cfcd01189a
skeetch <ben.oliverson@gmail.com>
2019-12-07 09:56:33 -0700

    Initial Commit

---
+++ PeopleOfStarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeopleOfStarWars
+//
+//  Created by Ben Oliverson on 12/7/19.
+//  Copyright © 2019 Ben Oliverson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
